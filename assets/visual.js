fedf2f0eaeecf410a01979c9610c2ce624f639a7
Akito Iwakura <aki@asterleen.com>
2018-09-10 13:57:35 +0300

    Add fullscreen visuals

--- assets/visual.js
+++ assets/visual.js
@@ -1,22 +1,48 @@
-var audioCtx,
+// Global vars.
+// Haters gonna hate, otters gonna ott.
+
+var
+	audioCtx,
 	audioEl,
 	analyser,
 	bufferLength,
 	canvasCtx,
 	source,
 	dataArray,
+	fullscreenRunning = false,
 	visualActive = false,
 	WIDTH  = 600,
-	HEIGHT = 100;
+	HEIGHT = 100,
+	FS_WIDTH = 0,
+	FS_HEIGHT = 0;
+
+
+function launchFullScreen(element) {
+  if(element.requestFullScreen) {
+    element.requestFullScreen();
+  } else if(element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if(element.webkitRequestFullScreen) {
+    element.webkitRequestFullScreen();
+  } else console.warn('No fullscreen support!');
+}
+
+function cancelFullscreen() {
+  if(document.cancelFullScreen) {
+    document.cancelFullScreen();
+  } else if(document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if(document.webkitCancelFullScreen) {
+    document.webkitCancelFullScreen();
+  }
+}
 
 function visualInit()
 {
-	var canvas = document.getElementById('player-vis');
+	FS_HEIGHT = window.screen.height;
+	FS_WIDTH = window.screen.width;
 
-	canvas.width = WIDTH;
-	canvas.height = HEIGHT;
-
-	canvasCtx = canvas.getContext('2d');
+	canvasInit();
 	
 	audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 	analyser = audioCtx.createAnalyser();
@@ -26,6 +52,33 @@ function visualInit()
 	analyser.fftSize = 2048;
 	bufferLength = analyser.frequencyBinCount;
 	dataArray = new Uint8Array(bufferLength);
+
+	document.addEventListener("webkitfullscreenchange", onFullscreenToggle);
+	document.addEventListener("mozfullscreenchange",    onFullscreenToggle);
+	document.addEventListener("fullscreenchange",       onFullscreenToggle);
+
+	$(document).dblclick(function(event) { launchFullScreen(document.getElementById('fullscreen-visuals')) });
+	$('#fullscreen-visuals').dblclick(function(event) { cancelFullscreen(); });
+}
+
+function canvasInit() {
+	var canvas = document.getElementById('player-vis' + ((fullscreenRunning) ? '-fs' : ''));
+
+	console.log (fullscreenRunning);
+
+	console.log('player-vis' + ((fullscreenRunning) ? '-fs' : ''), canvas);
+
+	canvas.width =  (fullscreenRunning) ? FS_WIDTH : WIDTH;
+	canvas.height = (fullscreenRunning) ? FS_HEIGHT : HEIGHT;
+	canvasCtx = canvas.getContext('2d');
+
+	canvasCtx.strokeStyle = (fullscreenRunning) ? 'rgb(171, 17, 193)' : 'rgba(81, 6, 92, 0.95)';
+	canvasCtx.lineWidth = 2;
+
+	if (fullscreenRunning)
+		$('#fullscreen-visuals').show();
+	else
+		$('#fullscreen-visuals').hide();
 }
 
 function visualStart() {
@@ -43,33 +96,33 @@ function visualStop() {
 	
 }
 
-function draw()
-{
+function draw() {
+
+	var localHeight = (fullscreenRunning) ? FS_HEIGHT : HEIGHT,
+		localWidth  = (fullscreenRunning) ? FS_WIDTH  : WIDTH;
+
 	if (visualActive && settings.visualsActive)
 		requestAnimationFrame(draw);
 	else { // clean up the canvas 
 		canvasCtx.fillStyle = 'rgb(0, 0, 0)';
-		canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+		canvasCtx.fillRect(0, 0, localWidth, localHeight);
 		return;
 	}
 
 	analyser.getByteTimeDomainData(dataArray);
 	
 	canvasCtx.fillStyle = 'rgba(0, 0, 0, 0.07)';
-	canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
-
-	canvasCtx.strokeStyle = 'rgba(81, 6, 92, 0.95)';
-	canvasCtx.lineWidth = 2;
+	canvasCtx.fillRect(0, 0, localWidth, localHeight);
 
 	canvasCtx.beginPath();
-	var sliceWidth = WIDTH * 1.0 / bufferLength;
+	var sliceWidth = localWidth * 1.0 / bufferLength;
 	
 	var x = 0;
 
 	for (var i = 0; i < bufferLength; i++) {
 
 		var v = dataArray[i] / 128.0,
-			y = v * HEIGHT / 2;
+			y = v * localHeight / 2;
 
 		if(i === 0) {
 			canvasCtx.moveTo(x, y);
@@ -80,8 +133,19 @@ function draw()
 		x += sliceWidth;
     }
 
-    canvasCtx.lineTo(WIDTH, HEIGHT / 2);
+    canvasCtx.lineTo(localWidth, localHeight / 2);
     canvasCtx.stroke();
+}
+
+function onFullscreenToggle(event) {
+	var fse = 
+		document.fullscreenElement || 
+		document.mozFullScreenElement || 
+		document.webkitFullscreenElement;
+
+	fullscreenRunning = (fse) ? true : false;
+
+	canvasInit();
 }
 
 function error (e) {
